9be4d9a992fdbd48362a5fb10d9f6361cfb11f9e
NicolasGrosjean <grosjen@hotmail.fr>
2022-04-08 21:28:04 +0200

    Start conflating guideposts
    Start applying https://github.com/JVillafruela/atelier-osm-conflator on guideposts

---
+++ README.md
@@ -0,0 +1,49 @@
+# Scripts for maintaining hiking OpenStreetMap data in Grenoble Alpes Métropole
+
+## Installation
+
+I recommend to install a Python environment with conda, virtualenv or pipenv.
+
+### Create a Conda environment
+For example with conda, 
+[download and install miniconda](https://docs.conda.io/en/latest/miniconda.html)
+
+Create a conda environment
+
+```
+conda create -n osm_gam_hiking python
+```
+
+Activate the conda environment
+
+```
+activate osm_gam_hiking
+```
+
+OR according your operating system
+
+```
+conda activate osm_gam_hiking
+```
+
+### Install requirements
+
+Whatever your Python setup, install the requirement packages with the following command:
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Download the [Grenoble Alpes Métropole dataset](https://data.metropolegrenoble.fr/ckan/dataset/espnat_sentier_poteaux_metro) and put it in *data* directory.
+
+It can be renamed with geojson extension like it is a geojson which can be read in https://geojson.io.
+
+```
+conflate guideposts.py -i data/POTEAUX_DIRECTIONNELS_SUR_SENTIERS_BALISES_EPSG4326_2022_02_14.geojson -o data/guidepost.osm -c data/guidepost.geojson
+```
+
+## License
+
+The project has an [Apache-2.0 License](LICENSE).
---
+++ guideposts.py
@@ -0,0 +1,45 @@
+# Adaptation from https://github.com/JVillafruela/atelier-osm-conflator/blob/master/osm-conflator/arceaux.py
+
+# Extract name and elevation
+source = "Grenoble-Alpes Métropole"
+no_dataset_id = True
+query = [("information", "guidepost")]
+master_tags = ("ref", "operator", "name", "ele")
+max_distance = 20
+duplicate_distance = 0
+
+
+def dataset(fileobj):
+    import codecs
+    import json
+    import logging
+
+    source = json.load(codecs.getreader("utf-8")(fileobj))
+    data = []
+    for el in source["features"]:
+        try:
+            lon = el["geometry"]["coordinates"][0]
+            lat = el["geometry"]["coordinates"][1]
+            gid = el["properties"]["ogc_fid"]
+            name = el["properties"]["nommobilier"]
+            ele = el["properties"]["blaltitude"]
+            if ele is not None:
+                # Manage value like "845 m"
+                ele = ele.split(" ")[0]
+            tags = {
+                "information": "guidepost",
+                "tourism": "information",
+                "name": name,
+                "ele": ele,
+            }
+            try:
+                lat = float(lat)
+                lon = float(lon)
+                data.append(SourcePoint(gid, lat, lon, tags))
+            except Exception as e:
+                logging.warning(
+                    "PROFILE: Failed to parse lat/lon for gid %s: %s", gid, str(e)
+                )
+        except Exception as e:
+            logging.warning("PROFILE: Failed to get attributes for gid: %s", str(e))
+    return data
